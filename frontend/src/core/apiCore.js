8072c623bb98273c9c78eac307caeff143dd5bbd
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-09-22 20:19:46 +0530

    Add to Cart done.

--- frontend/src/core/apiCore.js
+++ frontend/src/core/apiCore.js
@@ -8,9 +8,7 @@ export const getProducts = (sortBy) => {
     .then((response) => {
       return response.json()
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
 
 export const getCategories = () => {
@@ -20,9 +18,7 @@ export const getCategories = () => {
     .then((response) => {
       return response.json()
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
 
 export const getFilteredProducts = (skip, limit, filters = {}) => {
@@ -56,9 +52,7 @@ export const list = (params) => {
     .then((response) => {
       return response.json()
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
 
 export const read = (productId) => {
@@ -68,9 +62,7 @@ export const read = (productId) => {
     .then((response) => {
       return response.json()
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
 
 export const listRelated = (productId) => {
@@ -80,7 +72,52 @@ export const listRelated = (productId) => {
     .then((response) => {
       return response.json()
     })
-    .catch((err) => {
-      console.log(err)
+    .catch((err) => console.log(err))
+}
+
+export const getBraintreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json()
     })
+    .catch((err) => console.log(err))
+}
+
+export const processPayment = (userId, token, paymentData) => {
+  return fetch(`${API}/braintree/payment/${userId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(paymentData),
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
+}
+
+export const createOrder = (userId, token, createOrderData) => {
+  return fetch(`${API}/order/create/${userId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ order: createOrderData }),
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
 }
