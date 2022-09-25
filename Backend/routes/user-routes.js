5ab0a2fb12330b7cf6debeed798ebec2ebd171f9
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-09-25 13:53:53 +0530

    User can see purchase history.

--- Backend/routes/user-routes.js
+++ Backend/routes/user-routes.js
@@ -6,7 +6,12 @@ const {
   isAdmin,
   isAuth,
 } = require('../controllers/auth-controllers')
-const { userById, read, update } = require('../controllers/user-controllers')
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory,
+} = require('../controllers/user-controllers')
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
@@ -16,6 +21,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read)
 router.put('/user/:userId', requireSignin, isAuth, update)
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory)
 
 router.param('userId', userById)
 
